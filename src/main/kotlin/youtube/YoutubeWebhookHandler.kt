66f3b975172da94d2666413d58d1a146656fb8c9
jbeck18 <j.beck18@me.com>
2020-10-04 16:26:18 -0400

    Remove all files in .gitignore

--- src/main/kotlin/youtube/YoutubeWebhookHandler.kt
+++ src/main/kotlin/youtube/YoutubeWebhookHandler.kt
@@ -1,5 +1,6 @@
 package youtube
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.google.inject.Inject
 import io.javalin.http.Context
 import io.javalin.http.Handler
@@ -8,7 +9,7 @@ import io.javalin.http.Handler
  *  A Javalin handler that handles callbacks from YouTube.
  *  https://developers.google.com/youtube/v3/guides/push_notifications
  *
- *  This class needs to handle the following three cases:
+ *  This endpoint needs to handle the following three cases:
  *      1. A new video is uploaded
  *      2. An existing video's title is updated
  *      3. An existing video's description is updated
@@ -16,6 +17,9 @@ import io.javalin.http.Handler
 class YoutubeWebhookHandler @Inject constructor(
     private val videoEventListener: YoutubeVideoEventListener
 ) : Handler {
+
+    // Jackson xml parser
+    private val xmlParser = XmlMapper()
 
     override fun handle(ctx: Context) {
         TODO("Not yet implemented")
